bc616998785f1c3f00a8c5782c4dc5b0da248bc0
Joe Ortiz <joeftw18@gmail.com>
2019-07-10 20:55:15 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message){
+        this.message  = message;
+    }
+    public void runnable(){
+        System.out.println("\nYour Message : " + message);
+    }
+}
